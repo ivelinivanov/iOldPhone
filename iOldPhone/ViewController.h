e346ad8f3822a2241bdec4f5d23865e6071a279b
Ivelin Ivanov <ivelin.ivanov@Student01s-Mac-mini.local>
2013-08-16 12:11:16 +0300

    Initial Commit

---
+++ iOldPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOldPhone
+//
+//  Created by Ivelin Ivanov on 8/16/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
